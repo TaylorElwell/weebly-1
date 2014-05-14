6617de8e0cf8b9296917b101afe1e51d80845da9
Joshua Beitler <joshbeitler@gmail.com>
2014-05-14 16:43:33 -0600

    Split create site implementation into its own file

--- lib/weebly.rb
+++ lib/weebly.rb
@@ -1,11 +1,2 @@
 require 'weebly/version'
-require 'zip'
-require 'colorize'
-
-module Weebly
-  class Weebly
-    def self.create_site(sitename)
-      Dir.mkdir sitename ||= puts 'Already exists'
-    end
-  end
-end
+require 'weebly/create_site'
